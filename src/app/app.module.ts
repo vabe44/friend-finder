da5f4291a1fe4efe091a0f01d6ef86fd28cfbf19
Benedek Vass <vabe44@gmail.com>
2018-03-20 16:02:03 +0100

    added splash screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashScreenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: SplashScreenComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
